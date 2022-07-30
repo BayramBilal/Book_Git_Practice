1a10663a84bfeef5ae3e65f3a89a628f2fd49339
BayramBilal <bilalbayramb22@gmail.com>
2022-07-30 13:02:37 +0300

    verify price

--- Src/day20/TestCase100.java
+++ Src/day20/TestCase100.java
@@ -1,9 +1,11 @@
 package day20;
 
-public class TestCase100 {,
+public class TestCase100 {
 
     // Testing for check out
 
     // Added 2 items to rhe cart
+
+    // Verify the total price of the item
 
 }
